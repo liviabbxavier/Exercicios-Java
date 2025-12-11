98bdbc066195eca3fa4ac1e2c7cc62a195a75c33
liviabbxavier <liviabrocardo43@gmail.com>
2025-12-11 09:53:49 -0300

    Add files via upload

--- CalculadoraIMC/CalculaIMC.java
+++ CalculadoraIMC/CalculaIMC.java
@@ -1,15 +1,21 @@
 package CalculadoraIMC;
 
+import java.util.Scanner;
+
 public class CalculaIMC {
     // fórmula: IMC = peso / altura²
-        double p = 65.5;
-        double h = 1.63;
+    // double p = 65.5;
+    // double h = 1.63;
+    // 
 
-        public void calculaImc() {
-            double imc = (p / (h * h));
-            System.out.println("Seu peso é: " + p);
-            System.out.println("Sua altura é: " + h);
-            System.out.println("Fazendo o cálculo IMC...");
-            System.out.println(String.format("O valor do seu IMC é: %.1f", imc));
-        }
+    public void calculaImc() {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o seu peso: ");
+        double p = leitura.nextDouble();
+        System.out.println("Digite sua altura: ");
+        double h = leitura.nextDouble();
+        System.out.println("Fazendo o cálculo IMC...");
+        double imc = (p / (h * h));
+        System.out.println(String.format("Sua massa corporal é: %.2f", imc));
+    }
 }
