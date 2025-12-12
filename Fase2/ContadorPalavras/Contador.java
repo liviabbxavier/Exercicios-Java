1d2f9f8889e2087b4e4c1fe29099e71767790abc
liviabbxavier <liviabrocardo43@gmail.com>
2025-12-12 11:05:25 -0300

    Add files via upload

--- Fase2/ContadorPalavras/Contador.java
+++ Fase2/ContadorPalavras/Contador.java
@@ -1,5 +1,41 @@
 package Fase2.ContadorPalavras;
 
+import java.util.Scanner;
+
 public class Contador {
-    
+    private int vogais;
+    private int consoantes;
+
+    Scanner leitura = new Scanner(System.in);
+
+    public void contagem() {
+        System.out.println("Escreva uma frase: ");
+        String frase = leitura.nextLine().toLowerCase();
+        // toLowerCase() deixa as letras em minúsculo
+
+        String[] palavras = frase.trim().split("\\s+");
+        int numero = palavras.length;
+
+        // trim() remove espaços no início e no fim
+        // split("\\s+") divide o texto por um ou mais espaços
+        // palavras.length faz a contagem de palavras
+
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+
+            if (c >= 'a' && c <= 'z') { // verifica se é letra
+                if ("aeiou".indexOf(c) != -1) {
+                    vogais++;
+                } else {
+                    consoantes++;
+                }
+            }
+        }
+
+        String inverte = new StringBuilder(frase).reverse().toString();
+
+        System.out.println("Sua frase tem " + numero + " palavras.");
+        System.out.println(vogais + " vogais e " + consoantes + " consoantes.");
+        System.out.println("Sua frase invertida ficaria assim: " + inverte);
+    }
 }
--- Fase2/ContadorPalavras/Principal.java
+++ Fase2/ContadorPalavras/Principal.java
@@ -2,6 +2,7 @@ package Fase2.ContadorPalavras;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        Contador fazContagem = new Contador();
+        fazContagem.contagem();
     }
 }
