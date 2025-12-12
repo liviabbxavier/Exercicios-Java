95fd53af51b5e586bfbf361da223e82747ce335c
liviabbxavier <liviabrocardo43@gmail.com>
2025-12-12 11:49:00 -0300

    Add files via upload

--- Fase2/ListaDeCompras/Lista.java
+++ Fase2/ListaDeCompras/Lista.java
@@ -1,5 +1,63 @@
 package Fase2.ListaDeCompras;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lista {
-    
+    private int escolha;
+    private String item;
+    Scanner leitura = new Scanner(System.in);
+
+    ArrayList<String> lista = new ArrayList<>();
+
+    public void listaDeCompras() {
+        do {
+            System.out.println("---Lista de compras---");
+            System.out.println("---Escolha uma das opções abaixo---");
+            System.out.println("1 - Adicionar um item");
+            System.out.println("2 - Remover um item da lista");
+            System.out.println("3 - Ver a quantidade de itens");
+            System.out.println("4 - Ver os itens da lista");
+            System.out.println("5 - Sair");
+            escolha = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (escolha) {
+                case 1:
+                    adicionaItem();
+                    break;
+                case 2:
+                    removeItem();
+                    break;
+                case 3:
+                    System.out.println("Sua lista possui " + lista.size() + " item(ns).");
+                    break;
+                case 4:
+                    System.out.println("Itens na lista: \n" + toString);
+                    break;
+                default:
+                    break;
+            }
+        } while (escolha != 5);
+        System.out.println("Programa escerrado!");
+    }
+
+    public void adicionaItem() {
+        System.out.println("Digite o item para adicionar...");
+        item = leitura.nextLine();
+        lista.add(item);
+        System.out.println("Item adicionado à lista com sucesso!");
+    }
+
+    public void removeItem() {
+        System.out.println("Digite o item que você deseja remover...");
+        item = leitura.nextLine();
+        lista.remove(item);
+        System.out.println("Item removido com sucesso!");
+    }
+
+    @Override
+    public String toString() {
+        return String.join("\n", lista);
+    }
 }
--- Fase2/ListaDeCompras/Principal.java
+++ Fase2/ListaDeCompras/Principal.java
@@ -2,6 +2,7 @@ package Fase2.ListaDeCompras;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        Lista minhaLista = new Lista();
+        minhaLista.listaDeCompras();
     }
 }
